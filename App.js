99a10dd578687312a2111fe43f80fa46e747870b
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2017-10-21 21:02:44 +0530

    Initilal App Layout

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+} from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
-  render() {
+// const PHOTOS = Array.from({ length: 24 }).map((_, i) => `https://unsplash.it/600/600/?random&__id${i}`);
+// const SCREEN = Dimensions.get('window');
+// const BOX_SIZE = (Dimensions.get('window').width / 2) - 12;
+
+export default class App extends Component {
+
+  render() {    
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MyStatusBar backgroundColor="green" barStyle="light-content" />
+        <View style={styles.appBar}>
+          <Text>Here</Text>
+        </View>
+        <View style={styles.content}></View>
       </View>
     );
   }
 }
 
+const MyStatusBar = ({backgroundColor, ...props}) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar backgroundColor={backgroundColor} {...props} />
+  </View>
+);
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor:'green',
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#33373B',
   },
 });
